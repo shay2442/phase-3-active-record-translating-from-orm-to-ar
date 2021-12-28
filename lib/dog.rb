0f4c645b1a8915f0c889e2785341b25789b0ddc3
shaywacker <shay2442@yahoo.com>
2021-12-28 13:26:53 -0600

    commit tree adf29a067fcad9887dfc3c445ea0aea9efee94f6

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,29 @@
-class Dog
-end
+require "pry"
+require "active_record"
+
+# Setup a database connection
+ActiveRecord::Base.establish_connection(
+  adapter: "sqlite3",
+  database: "db/dogs.sqlite"
+)
+
+
+# Create a Students table
+sql = <<-SQL
+  CREATE TABLE IF NOT EXISTS dogs (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    breed TEXT
+  )
+SQL
+ActiveRecord::Base.connection.execute(sql)
+
+# Log SQL output to the terminal
+ActiveRecord::Base.logger = Logger.new(STDOUT)
+
+# Have the Student class inherit from ActiveRecord::Base
+class Dog < ActiveRecord::Base
+end 
+
+# binding.pry
+# ""
